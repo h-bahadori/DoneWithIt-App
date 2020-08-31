7c8a81bf963b924c688819cc9f38cdf5fe70ac06
eryson-prog <59797176+eryson-prog@users.noreply.github.com>
2020-08-31 15:46:18 +0430

    Update Screens

--- App.js
+++ App.js
@@ -1,39 +1,6 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Switch } from 'react-native';
-import Screen from './app/components/Screen';
-import Icon from './app/components/Icon';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import AppPicker from './app/components/AppPicker';
-
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Cloting", value: 2 },
-  { label: "Cameras", value: 3 },
-]
+import React from "react";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-
-  return (
-
-
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={item => setCategory(item)}
-        items={categories} icon="apps" placeholder="Category" />
-      <AppTextInput icon="email" placeholder="email" />
-
-
-    </Screen>
-
-
-  );
+  return <ListingEditScreen />;
 }
-
--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,79 +1,71 @@
-import React from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import React from "react";
+import { StyleSheet, View, FlatList } from "react-native";
 
-import Screen from '../components/Screen';
-import ListItem from '../components/ListItem';
-import colors from '../config/colors';
-import Icon from '../components/Icon';
-import ListItemSeparator from '../components/ListItemSeparator';
+import Screen from "../components/Screen";
+import { ListItem, ListItemSeparator } from "../components/lists";
+import colors from "../config/colors";
+import Icon from "../components/Icon";
 
 const menuItems = [
-    {
-        title: "My Listing",
-        icon: {
-            name: "format-list-bulleted",
-            backgroundColor: colors.primary
-
-        }
+  {
+    title: "My Listings",
+    icon: {
+      name: "format-list-bulleted",
+      backgroundColor: colors.primary,
     },
-    {
-        title: "My Messages",
-        icon: {
-            name: "email",
-            backgroundColor: colors.secondary
+  },
+  {
+    title: "My Messages",
+    icon: {
+      name: "email",
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 
-        }
-    }
-]
-
-const AccountScreen = () => {
-    return (
-        <Screen style={styles.screen}>
-            <View style={styles.container}>
-                <ListItem
-                    title="Hamed Bahadori"
-                    subTitle="h.bahadori79@gmail.com"
-                    image={require('../assets/hamed.jpg')}
-                />
-            </View>
-            <View style={styles.container}>
-                <FlatList
-                    data={menuItems}
-                    keyExtractor={menuItem => menuItem.title}
-                    ItemSeparatorComponent={ListItemSeparator}
-                    renderItem={({ item }) =>
-                        <ListItem
-                            title={item.title}
-                            IconComponent={
-                                <Icon name={item.icon.name} backgroundColor={item.icon.backgroundColor} />
-
-                            }
-                        />
-                    }
-                />
-
-            </View>
-
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Mosh Hamedani"
+          subTitle="programmingwithmosh@gmail.com"
+          image={require("../assets/mosh.jpg")}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          ItemSeparatorComponent={ListItemSeparator}
+          renderItem={({ item }) => (
             <ListItem
-                title="Log Out"
-                IconComponent={
-                    <Icon name="logout" backgroundColor="#ffe66d" />
-                }
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
             />
-
-
-        </Screen>
-    );
+          )}
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
+    </Screen>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    screen: {
-        backgroundColor: colors.light,
-    },
-    container: {
-        marginVertical: 20
-    }
+  screen: {
+    backgroundColor: colors.light,
+  },
+  container: {
+    marginVertical: 20,
+  },
+});
 
-})
 export default AccountScreen;
